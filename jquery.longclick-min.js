12915dad7af478012e799eedddc2a63428d2f8dc
paganaye <pascal2007@ganaye.com>
2014-10-13 16:34:19 +0200

    Update jquery.longclick-min.js

--- jquery.longclick-min.js
+++ jquery.longclick-min.js
@@ -1,12 +1,15 @@
-/*
- Longclick Event
- Copyright (c) 2010 Petr Vostrel (http://petr.vostrel.cz/)
- Dual licensed under the MIT (MIT-LICENSE.txt)
- and GPL (GPL-LICENSE.txt) licenses.
-
- Version: 0.3.2
- Updated: 2010-06-22
-*/
-(function(a){function n(b){a.each("touchstart touchmove touchend touchcancel".split(/ /),function(d,e){b.addEventListener(e,function(){a(b).trigger(e)},false)});return a(b)}function j(b){function d(){a(e).data(h,true);b.type=f;jQuery.event.handle.apply(e,o)}if(!a(this).data(g)){var e=this,o=arguments;a(this).data(h,false).data(g,setTimeout(d,a(this).data(i)||a.longclick.duration))}}function k(){a(this).data(g,clearTimeout(a(this).data(g))||null)}function l(b){if(a(this).data(h))return b.stopImmediatePropagation()||
-false}var p=a.fn.click;a.fn.click=function(b,d){if(!d)return p.apply(this,arguments);return a(this).data(i,b||null).bind(f,d)};a.fn.longclick=function(){var b=[].splice.call(arguments,0),d=b.pop();b=b.pop();var e=a(this).data(i,b||null);return d?e.click(b,d):e.trigger(f)};a.longclick={duration:500};a.event.special.longclick={setup:function(){/iphone|ipad|ipod/i.test(navigator.userAgent)?n(this).bind(q,j).bind([r,s,t].join(" "),k).bind(m,l).css({WebkitUserSelect:"none"}):a(this).bind(u,j).bind([v,
-w,x,y].join(" "),k).bind(m,l)},teardown:function(){a(this).unbind(c)}};var f="longclick",c="."+f,u="mousedown"+c,m="click"+c,v="mousemove"+c,w="mouseup"+c,x="mouseout"+c,y="contextmenu"+c,q="touchstart"+c,r="touchend"+c,s="touchmove"+c,t="touchcancel"+c,i="duration"+c,g="timer"+c,h="fired"+c})(jQuery);
+/**
+ * jQuery Longclick Event
+ * ======================
+ * Press & hold mouse button "long click" special event for jQuery 1.4.x
+ *
+ * @license Longclick Event
+ * Copyright (c) 2010 Petr Vostrel (http://petr.vostrel.cz/)
+ * Modified in 2014 by Pascal GANAYE (https://github.com/paganaye/Longclick/)
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ *
+ * Version: 0.4.0
+ * Updated: 2014-10-13
+ */
+!function(t){function n(n){return t.each("touchstart touchend touchcancel".split(/ /),function(i,e){n.addEventListener(e,function(){t(n).trigger(e)},!1)}),t(n)}function i(n){function i(){t(e).data(v,!0).data(m,null),n.type=o;var i=jQuery.event.dispatch||jQuery.event.handle;i.apply(e,a)}if(!t(this).data(m)){var e=this,a=arguments;t(this).data(v,!1).data(m,setTimeout(i,t(this).data(p)||t.longclick.duration))}}function e(){var n=t(this).data(m);n&&t(this).data(m,clearTimeout(n)||null)}function a(n){return t(this).data(v)?n.stopImmediatePropagation()||!1:void 0}t.fn.longclick=function(){var n=arguments[0],i=arguments[1],e=t(this).data(p,i||null);return n?e.bind(o,n):e.trigger(o)},t.longclick={duration:500},t.event.special.longclick={setup:function(){/iphone|ipad|ipod/i.test(navigator.userAgent)?n(this).bind(h,i).bind([g,f].join(" "),e).bind(r,a).css({WebkitUserSelect:"none"}):t(this).bind(c,i).bind([d,s,l].join(" "),e).bind(r,a)},teardown:function(){t(this).unbind(u)}};var o="longclick",u="."+o,c="mousedown"+u,r="click"+u,d="mouseup"+u,s="mouseout"+u,l="contextmenu"+u,h="touchstart"+u,g="touchend"+u,f="touchcancel"+u,p="duration"+u,m="timer"+u,v="fired"+u}(jQuery);
