1cce3ce92c9a7d626e8df91510cdb2108a452f7f
paganaye <pascal2007@ganaye.com>
2014-10-13 16:31:34 +0200

    Restore compatibilty with JQuery up to 2.1.0
    Reformatted following warnings from JSHint.
    Sample here: http://jsfiddle.net/paganaye/1njL9017/22/

--- jquery.longclick.js
+++ jquery.longclick.js
@@ -5,142 +5,136 @@
  *
  * @license Longclick Event
  * Copyright (c) 2010 Petr Vostrel (http://petr.vostrel.cz/)
+ * Modified in 2014 by Pascal GANAYE (https://github.com/paganaye/Longclick/)
  * Dual licensed under the MIT (MIT-LICENSE.txt)
  * and GPL (GPL-LICENSE.txt) licenses.
  *
- * Version: 0.3.2
- * Updated: 2010-06-22
- */
-(function($){
+ * Version: 0.4.0
+ * Updated: 2014-10-13
+ */ (function ($) {
+    /** `.longclick([ handler ], [ duration ])`
+     * If supplied, optional custom `duration` is used for target element(s).
+     * This method is a shortcut for `.trigger("longclick")` if called without arguments.
+     * Returns *jQuery*.
+     */
+    $.fn.longclick = function longclick() {
+        var handler = arguments[0],
+            duration = arguments[1],
+            $this = $(this).data(_duration_, duration || null);
+        /* Bind long click */
+        return handler ? $this.bind(type, handler) : $this.trigger(type);
+    };
 
-  /*
-  `.click( duration, handler )`
+    /**
+     *  Configuration
+     */
+    $.longclick = {
+        /**
+         * For how long (in milliseconds) mouse button must be pressed down (or touched) stationery
+         * to qualify as a *long click*.
+         * False value results in using the configured default.
+         * Default `duration` is **500** and is stored in `jQuery.longclick.duration` variable.
+         */
+        duration: 500
+    };
 
-  * Simply supply `duration` to the well-known `.click` method and you have a *long click*.
-  * This method is a shortcut for `.bind("longclick", handler)`.
-  * Returns *jQuery*.
-  */
-  var
-    $_fn_click= $.fn.click
+    /**
+     * Bindings
+     */
+    $.event.special.longclick = {
+        setup: function (data, namespaces) {
+            if (!(/iphone|ipad|ipod/i).test(navigator.userAgent)) {
+                /* normal technique for standard mouse-based interaction */
+                $(this)
+                    .bind(_mousedown_, schedule)
+                    .bind([_mouseup_, _mouseout_, _contextmenu_].join(' '), cancel)
+                    .bind(_click_, click);
+            } else {
+                /* and special handling for touch-based interaction on iPhone-compatibile devices */
+                touch_enabled(this)
+                    .bind(_touchstart_, schedule)
+                    .bind([_touchend_, _touchcancel_].join(' '), cancel)
+                    .bind(_click_, click)
+                    .css({
+                    WebkitUserSelect: 'none'
+                });
+            }
+        },
+        teardown: function (namespaces) {
+            $(this).unbind(namespace);
+        }
+    };
 
-  $.fn.click= function click(duration, handler){
-    /* Shortcircuit ordinary click calls */
-    if (!handler) return $_fn_click.apply(this, arguments)
-    /* Bind long click */
-    return $(this).data(_duration_, duration || null).bind(type, handler)
-  }
+    /**
+     * Commit subset of touch events to trigger jQuery events of same names
+     */
+    function touch_enabled(element) {
+        $.each('touchstart touchend touchcancel'.split(/ /), function bind(ix, it) {
+            element.addEventListener(it, function trigger_jquery_event(event) {
+                $(element).trigger(it);
+            }, false);
+        });
+        return $(element);
+    }
 
-  /*
-  `.longclick( [ duration ], [ handler ] )`
+    /**
+     * Handlers
+     */
+    function schedule(event) {
+        /* Check the timer isn't already running and drop if so */
+        if ($(this).data(_timer_)) return;
+        /* Catch in closure the `this` reference and `arguments` for later */
+        var
+        element = this,
+            args = arguments;
 
-  * If supplied, optional custom `duration` is used for target element(s).
-  * This method is a shortcut for `.click(duration, handler)` when at least `handler` is supplied
-    and for `.trigger("longclick")` if called without arguments.
-  * Returns *jQuery*.
-  */
-  $.fn.longclick= function longclick(){
-    var
-      args= [].splice.call(arguments, 0),
-      handler= args.pop(),
-      duration= args.pop(),
-      $this= $(this).data(_duration_, duration || null)
-    return handler ? $this.click(duration, handler) : $this.trigger(type)
-  }
+            function scheduled() {
+                /* Flag as "fired" and rejoin the default event flow */
+                $(element).data(_fired_, true)
+                    .data(_timer_, null);
+                event.type = type;
+                var f = (jQuery.event.dispatch || jQuery.event.handle);
+                f.apply(element, args);
+            }
+            /* Flag as "not fired" and schedule the trigger */
+            $(this)
+                .data(_fired_, false)
+                .data(_timer_, setTimeout(scheduled, $(this).data(_duration_) || $.longclick.duration));
 
-  /*
-  Configuration
-  */
-  $.longclick= {
+    }
+
+    function cancel(event) {
+        var timer = $(this).data(_timer_);
+        if (timer) {
+            /* cancel the scheduled trigger */
+            $(this).data(_timer_, clearTimeout(timer) || null);
+        }
+    }
+
+    function click(event) {
+        /* Prevent `click` event to be fired after button release once `longclick` was fired */
+        if ($(this).data(_fired_)) return event.stopImmediatePropagation() || false;
+    }
+
     /*
-    * For how long (in milliseconds) mouse button must be pressed down (or touched) stationery
-      to qualify as a *long click*.
-    * False value results in using the configured default.
-    * Default `duration` is **500** and is stored in `jQuery.longclick.duration` variable.
-    */
-    duration: 500
-  }
+     * Frequent primitives and shortcuts
+     */
+    var type = 'longclick',
+        namespace = '.' + type,
 
-  /*
-  Bindings
-  */
-  $.event.special.longclick= {
-    setup: function(data, namespaces){
-      if (!(/iphone|ipad|ipod/i).test(navigator.userAgent)){
-        /* normal technique for standard mouse-based interaction */
-        $(this)
-        .bind(_mousedown_, schedule)
-        .bind([_mouseup_, _mouseout_, _contextmenu_].join(' '), annul)
-        .bind(_click_, click)
-      }else{
-        /* and special handling for touch-based interaction on iPhone-compatibile devices */
-        touch_enabled(this)
-        .bind(_touchstart_, schedule)
-        .bind([_touchend_, _touchmove_, _touchcancel_].join(' '), annul)
-        .bind(_click_, click)
-        .css({ WebkitUserSelect: 'none' })
-      }
-    },
-    teardown: function(namespaces){
-      $(this).unbind(namespace)
-    }
-  }
+        /* Event strings */
+        _mousedown_ = 'mousedown' + namespace,
+        _click_ = 'click' + namespace,
+        _mouseup_ = 'mouseup' + namespace,
+        _mouseout_ = 'mouseout' + namespace,
+        _contextmenu_ = 'contextmenu' + namespace,
+        _touchstart_ = 'touchstart' + namespace,
+        _touchend_ = 'touchend' + namespace,
+        _touchcancel_ = 'touchcancel' + namespace,
 
-  /*
-  Commit subset of touch events to trigger jQuery events of same names
-  */
-  function touch_enabled(element){
-    $.each('touchstart touchmove touchend touchcancel'.split(/ /), function bind(ix, it){
-      element.addEventListener(it, function trigger_jquery_event(event){ $(element).trigger(it) }, false);
-    });
-    return $(element);
-  }
-
-  /*
-  Handlers
-  */
-  function schedule(event){
-    /* Check the timer isn't already running and drop if so */
-    if ($(this).data(_timer_)) return;
-    /* Catch in closure the `this` reference and `arguments` for later */
-    var
-      element= this,
-      args= arguments
-    /* Flag as "not fired" and schedule the trigger */
-    $(this)
-    .data(_fired_, false)
-    .data(_timer_, setTimeout(scheduled, $(this).data(_duration_) || $.longclick.duration))
-
-    function scheduled(){
-      /* Flag as "fired" and rejoin the default event flow */
-      $(element).data(_fired_, true)
-      event.type= type
-      jQuery.event.handle.apply(element, args)
-    }
-  }
-  function annul(event){
-    /* Annul the scheduled trigger */
-    $(this).data(_timer_, clearTimeout($(this).data(_timer_)) || null)
-  }
-  function click(event){
-    /* Prevent `click` event to be fired after button release once `longclick` was fired */
-    if ($(this).data(_fired_)) return event.stopImmediatePropagation() || false
-  }
-
-  /*
-  Frequent primitives and shortcuts
-  */
-  var
-    type= 'longclick',
-    namespace= '.' + type,
-
-    /* Event strings */
-    _mousedown_= 'mousedown'+namespace, _click_= 'click'+namespace,
-    _mousemove_= 'mousemove'+namespace, _mouseup_= 'mouseup'+namespace,
-    _mouseout_= 'mouseout'+namespace, _contextmenu_= 'contextmenu'+namespace,
-    _touchstart_= 'touchstart'+namespace, _touchend_= 'touchend'+namespace,
-    _touchmove_= 'touchmove'+namespace, _touchcancel_= 'touchcancel'+namespace,
-
-    /* Storage keys */
-    _duration_= 'duration'+namespace, _timer_= 'timer'+namespace, _fired_= 'fired'+namespace
+        /* Storage keys */
+        _duration_ = 'duration' + namespace,
+        _timer_ = 'timer' + namespace,
+        _fired_ = 'fired' + namespace;
 
 })(jQuery);
